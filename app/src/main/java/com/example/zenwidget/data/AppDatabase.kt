7e81fcbd1f3bbba910be5bccd96bcb943a294082
Lim-ZY <tcmpiano03@gmail.com>
2026-06-30 19:49:05 +0800

    Add default quotes and actions

--- app/src/main/java/com/example/zenwidget/data/AppDatabase.kt
+++ app/src/main/java/com/example/zenwidget/data/AppDatabase.kt
@@ -5,6 +5,10 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Database(entities = [RepoItem::class], version = 1, exportSchema = false)
 @TypeConverters(RepoTypeConverters::class)
@@ -21,9 +25,59 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "zen_database"
-                ).build()
+                ).addCallback(DatabaseCallback()).build()
                 INSTANCE = instance
                 instance
+            }
+        }
+
+        private class DatabaseCallback : RoomDatabase.Callback() {
+            override fun onCreate(db: SupportSQLiteDatabase) {
+                super.onCreate(db)
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    INSTANCE?.let { database ->
+                        populateDatabase(database.zenDao())
+                    }
+                }
+            }
+
+            suspend fun populateDatabase(dao: ZenDao) {
+                dao.insertItem(
+                    RepoItem(
+                        repoType = RepoType.QUOTES,
+                        text = "The only way to do great work is to love what you do.",
+                        caption = "Steve Jobs"
+                    )
+                )
+                dao.insertItem(
+                    RepoItem(
+                        repoType = RepoType.QUOTES,
+                        text = "真诚清净平等正觉慈悲，看破放下自在随缘念佛。",
+                        caption = "净空法师"
+                    )
+                )
+                dao.insertItem(
+                    RepoItem(
+                        repoType = RepoType.QUOTES,
+                        text = "夫物速成则疾亡，晚就则善终。 朝华之草，夕而零落；松柏之茂，隆寒不衰。 是以大雅君子恶速成。",
+                        caption = "群书治要"
+                    )
+                )
+                dao.insertItem(
+                    RepoItem(
+                        repoType = RepoType.ACTIONS,
+                        text = "Drink some water.",
+                        caption = "Relax~"
+                    )
+                )
+                dao.insertItem(
+                    RepoItem(
+                        repoType = RepoType.ACTIONS,
+                        text = "Take a few slow, deep breaths. Notice your belly rising, and falling.",
+                        caption = "Mindfulness~"
+                    )
+                )
             }
         }
     }
